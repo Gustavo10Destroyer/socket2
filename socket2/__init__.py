4b9a7b432c36a794d2dc50936cdcb9dbafa84b5d
Gustavo10Destroyer <66029954+Gustavo10Destroyer@users.noreply.github.com>
2021-12-21 17:18:08 -0300

    Create __init__.py

---
+++ socket2/__init__.py
@@ -0,0 +1,7 @@
+from .utils import Socket2
+
+__license__ = "MIT"
+__version__ = "4.2.1"
+__title__ = "Socket2"
+__author__ = "Gustavo10Destroyer"
+__copyright__ = "Copyright 2021 Gustavo10Destroyer"
